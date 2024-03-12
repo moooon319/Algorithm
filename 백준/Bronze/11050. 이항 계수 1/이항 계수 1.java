167a2ab4681e5bdaf45a133a9064224bce42d446
moooon <91143264+moooon319@users.noreply.github.com>
2024-03-12 17:02:49 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,28 @@
+//package Algorithm;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	int n = Integer.parseInt(st.nextToken());
+	int k = Integer.parseInt(st.nextToken());
+	
+	System.out.println(fact(n)/(fact(k)*fact(n-k)));
+	}
+	
+
+	static int fact(int num) {
+		int result = 1;
+		
+		for(int i=1;i<=num;i++) {
+			result*=i;
+			
+		}
+		return result;
+	}
+	}
+
