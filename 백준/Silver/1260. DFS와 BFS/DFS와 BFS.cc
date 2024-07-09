cf61a64eb8cc6f18f4537f74549a7c982d328a91
moooon <91143264+moooon319@users.noreply.github.com>
2024-07-10 01:59:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,64 +1,72 @@
-#include <iostream>
-#include <queue>
-#include <string.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void DFS(int);
+void BFS(int);
+
 int n, m, v;
+int a, b;
 int arr[1001][1001];
 bool visited[1001];
 queue<int> q;
 
-void DFS(int v)
-{
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+	cin >> n >> m >> v;
+
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+
+	DFS(v);
+	cout << '\n';
+	memset(visited, false, sizeof(visited));
+	//fill (visited, visited + 1001, false);
+	BFS(v);
+		
+
+	return 0;
+
+}
+void DFS(int v) {
+
 	visited[v] = true;
 	cout << v << ' ';
 
-	for (int i = 1; i <= n; i++) {
-		if (arr[v][i] == 1 && visited[i] == 0) { // v랑 연결되어 있고 방문x
+	for(int i=1; i<=n; i++){
+		if (arr[v][i] == 1 &&visited[i]==false)
 			DFS(i);
-		}
 	}
+
 }
 
-void BFS(int v)
-{
+void BFS(int v) {
 	q.push(v);
 	visited[v] = true;
 	cout << v << ' ';
 
 	while (!q.empty()) {
+
 		v = q.front();
 		q.pop();
 
-		for (int x = 1; x <= n; x++) {
-			if (arr[v][x] == 1 && visited[x] == 0)
-			{
-				q.push(x);
-				visited[x] = true;
-				cout << x << ' ';
+		for (int i = 1; i <= n; i++) {
+			if (arr[v][i] == 1 && visited[i] == false) {
+				q.push(i);
+				visited[i] = true;
+				cout << i << ' ';
 			}
 		}
+
 	}
-}
 
-int main()
-{
-	cin >> n >> m >> v;
 
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		arr[a][b] = 1;
-		arr[b][a] = 1; //양방향
-	}
-	DFS(v);
-	cout << '\n';
-
-	memset(visited, 0, sizeof(visited)); // 방문기록 초기화
-
-	BFS(v);
 
 }
-
-
-	
