7c22f44a4e01b18ec7c51a287bde3eb427bd4ba6
moooon <91143264+moooon319@users.noreply.github.com>
2024-07-28 14:47:10 +0900

    [Silver V] Title: 숫자 카드, Time: 204 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,41 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector <long> card;
+vector<int> v1;
 
-bool binarysearch(int start, int end, long num) {
-	while (start <= end) {
-		int mid = (start + end) / 2;
+int binarysearch(int left, int right, int target) {
 
-		if (card[mid] == num)
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (target == v1[mid]) {
 			return 1;
-		else if (card[mid] < num)
-			start = mid + 1;
-		else
-			end = mid - 1;
+		}
+		else if (target < v1[mid]) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
 	}
+	
 	return 0;
 }
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 
-	int n, m;
-	long key_1 = 0, key_2 = 0;
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m,num,num2;
+
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> key_1;
-		card.push_back(key_1);
+		cin >> num;
+		v1.push_back(num);
 	}
-	sort(card.begin(), card.end());
+
+	sort(v1.begin(), v1.end());
+
 	cin >> m;
 	for (int i = 0; i < m; i++) {
-		cin >> key_2;
-		cout << binarysearch(0, n - 1, key_2) << " ";
+		cin >> num2;
+		cout << binarysearch(0, n - 1, num2) << ' ';
 	}
-	return 0;
+	
 }
-
